77780da85e2d426adabef048fa2bce17d21c4b8e
Mohammed Noori <kmanoorib@gmail.com>
2018-06-14 19:43:51 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,12 +1,12 @@
 class Post
   attr_accessor :title, :author
-    @@all = []
+    @@all = [] #global variable set to an array
   def initialize(title)
     @title = title #assign argument to instance variable
     @@all << self #pushes new instance into a global variable upon initialization
   end
   
-  def self.all
+  def self.all #class method. returns an array of all post instances that have been created
     @@all
   end
   
